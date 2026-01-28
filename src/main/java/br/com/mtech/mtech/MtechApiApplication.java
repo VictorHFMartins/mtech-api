37864ee9cc91bd652af5cb9988f20cfd32d197f1
Victor <navictor70@gmail.com>
2026-01-28 11:49:07 -0300

    chore: bootstrap projeto Spring Boot

---
+++ src/main/java/br/com/mtech/mtech/MtechApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mtech.mtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtechApiApplication.class, args);
+	}
+
+}
